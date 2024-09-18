3633f52cfdd008388ea0f71b89502111de604745
Juan Diego Peña Gil <penagiljuandiego@gmail.com>
2024-09-18 02:19:50 -0400

    Fixing minor bugs

--- battleship.py
+++ battleship.py
@@ -1,13 +1,10 @@
-
 import random
 
 class Ship():
     def __init__(self, coordinates, length):
         self.coordinates = coordinates
         self.length = length
-        self.sunk = True
-    def __iter__(self):
-        return iter(self.coordinates)
+        self.sunk = False
     def __bool__(self): 
         return self.sunk
         
@@ -30,9 +27,9 @@ class Gameboard():
         self.opp_grid = [[0 for j in range(10)] for i in range(10)]
         self.own_ships = ships
     def __str__(self):
-        own_grid = [self.own_grid[i] for i in range(len(self.own_grid))]
-        opp_grid = [self.opp_grid[i] for i in range(len(self.opp_grid))]
-        return (own_grid, opp_grid) 
+        own_grid = '\n'.join(' '.join(str(cell) for cell in row) for row in self.own_grid)
+        opp_grid = '\n'.join(' '.join(str(cell) for cell in row) for row in self.opp_grid)
+        return f"Your grid:\n{own_grid}\n\nOpponent's grid:\n{opp_grid}"
     def place_ship(self, ship):
         for x, y in ship.coordinates:
             if not (0 <= x < 10 and 0 <= y < 10):
@@ -51,21 +48,20 @@ class Gameboard():
             self.own_grid[x][y] = "O"
             self.opp_grid[x][y] = "O"
             return "MISS"
-    def sunk_ships(self, ship): 
-        if all(self.own_grid[x][y] == "X" for x, y in ship.coordinates): return False
-        else: return True
+    def check_sunk_status(self, ship): 
+        if all(self.own_grid[x][y] == "X" for x, y in ship.coordinates): return True
+        else: return False
 
 class Player():
     def __init__(self, player = 0): # 0 is user | 1 for machine
         self.player = player
-        self.active = self.bool()
-        self.grid = Gameboard()
         self.ships = []
+        self.grid = Gameboard(self.ships)
         self.attack_record = []
     def get_coordinate(self):
         if self.player: 
             x, y = random.randint(0,9), random.randint(0,9)
-        else:
+        else: # Add 
             while True:
                 x = int(input("Enter starting x-coordinate (1-10): ")) - 1
                 y = int(input("Enter starting y-coordinate (1-10): ")) - 1
@@ -77,36 +73,43 @@ class Player():
     def input_ships(self): 
          for ship in [Two_Long, Three_Long, Three_Long, Four_Long, Five_Long]:
             print(f"Placing {ship.__name__}:")
-            x, y = self.get_coordinate()
             while True:
-                orientation = random.randint(0,1) if self.player else int(input("Enter orientation (0 for horizontal, 1 for vertical): "))
-                if orientation in [0, 1]:
-                    break
-                else: print("Invalid orientation value. Try again.")
-            delta_x, delta_y = (1, 0) if orientation == 0 else (0, 1)
-            coordinates = [(x + i * delta_x, y + i * delta_y) for i in range(ship.length)]
+                
+                while True:
+                    orientation = random.randint(0,1) if self.player else int(input("Enter orientation (0 for horizontal, 1 for vertical): "))
+                    if orientation in [0, 1]:
+                        break
+                    else: 
+                        print("Invalid orientation value. Try again.")
+                        continue
+                    
+                x, y = self.get_coordinate()
+                if (not (x in range(0, 10)) or not (y in range(0, 10))): ###
+                    print("Given coordinates out of bounds. Try again.")
+                    continue
+            
+                delta_x, delta_y = (1, 0) if orientation == 0 else (0, 1)
+                if x + (ship.length - 1) * delta_x >= 10 or y + (ship.length - 1) * delta_y >= 10:
+                    print("Ship placement goes out of bounds. Try again.")
+                    continue
 
-            while True: 
-                if (not (x in range(0, 9)) or not (y in range(0, 9)) or not (orientation in range(0, 1))): 
-                    print(...)
+                coordinates = [(x + i * delta_x, y + i * delta_y) for i in range(ship.length)]
+                    
+                if any((x + i * delta_x, y + i * delta_y) in [(coord[0], coord[1]) for ship in self.ships for coord in ship.coordinates] for i in range(ship.length)):
+                    print("Ship placement overlaps with another ship. Try again.")
                     continue
-                elif any((x + i * delta_x > 9 or y + i * delta_y > 9) for i in range(ship.length)):
-                    print(...)
-                    continue
-                elif set(coordinates).intersection(set(coord for ship in self.ships for coord in ship.coordinates)):
-                    print(...)
-                    continue
-                else: break
-            try:
-                ship_input = ship(coordinates)
-                self.ships.append(ship_input)
-            except ValueError as e:
-                print(f"Error placing ship: {e}")
+
+                try:
+                    ship_input = ship(coordinates)
+                    self.ships.append(ship_input)
+                    break
+                except ValueError as e:
+                    print(f"Error placing ship: {e}")
     def attack_coordinates(self): 
         while True:
             x, y = self.get_coordinate()
             if (x, y) in self.attack_record: 
-                print(...)
+                print("You already attacked there. Try again.")
                 continue
             else: 
                 self.attack_record.append((x, y))
@@ -116,9 +119,9 @@ class Player():
         return result
     def __bool__(self):
         for ship in self.ships:
-            ship.sunk = self.grid.sunk_ships(ship)
-        if all(ship.sunk for ship in self.ships): return False
-        else: return True
+            ship.sunk = self.grid.check_sunk_status(ship)
+        if all(ship.sunk for ship in self.ships): return True
+        else: return False
 
 def main():
     print("This is BATTLESHIP! recreated in Python.")
@@ -132,12 +135,7 @@ def main():
     while user and machine:
         print(f"Round {round}")
         if inning % 2 != 0:
-            own_grid, opp_grid = str(user.grid)
-            print(f"Your battlefield:")
-            print(own_grid)
-            print(f"Enemy's battlefield")
-            print(opp_grid)
-
+            print(user.grid)
             print("Attack!")
             result = user.attack(machine, user.attack_coordinates())
             print(f"The result was a... {result}")
